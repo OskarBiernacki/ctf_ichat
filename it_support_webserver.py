a37d5c2cf8cfbf2e4e5236143c3d7c56ec2dbb6a
05KAr8infoRn4cK1 <ozifozx@gmail.com>
2025-03-01 14:23:01 +0100

    Add admin user creation and remove old database on startup; refactor stylesheets for home and chat pages

--- it_support_webserver.py
+++ it_support_webserver.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template, redirect, url_for, request, flash
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager, UserMixin, login_user, login_required, logout_user, current_user
+import os
 
 app = Flask('chat_chat_chat_ctf')
 app.config['SECRET_KEY'] = 'your_secret_key'
@@ -13,6 +14,7 @@ class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(150), unique=True, nullable=False)
     password = db.Column(db.String(150), nullable=False)
+    is_admin = db.Column(db.Boolean, default=False)
 class Message(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     sender_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
@@ -27,7 +29,7 @@ def load_user(user_id):
 @app.route('/')
 @login_required
 def home():
-    return render_template('home.html', teststring='baka')
+    return render_template('home.html')
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
@@ -71,6 +73,15 @@ def user_chat(receiver_user_id):
     return render_template("chat.html", active_user=current_user.username)
 
 if __name__ == '__main__':
+    if os.path.exists('instance/users.db'):
+        print('Removing old database...')
+        os.remove('instance/users.db')
     with app.app_context():
         db.create_all()
+    
+        print('Creating admin user...')
+        admin = User(username='admin', password='admin', is_admin=True)
+        db.session.add(admin)
+        db.session.commit()
+
     app.run(debug=True)
